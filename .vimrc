3c8a6bd5e792cc469739f9d8c181cb4f1484acc2
Robin Ridler <robin.ridler@gmail.com>
2015-09-24 10:24:30 +0100

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,15 @@
+
+" Enable the pathogen plugin manager.
+execute pathogen#infect()
+
+" Enable syntax highlighting
+syntax on
+set background=dark
+
+
+filetype plugin indent on
+
+" Allow pasting in autoindent mode with F2
+set pastetoggle=<F2>
+
+let g:pymode_folding = 0
---
+++ .xmonad/xmobar.hs
@@ -0,0 +1,28 @@
+-- xmobar config used by Andrew Seidl, inspired by Vic Fryzel
+-- Author: Vic Fryzel
+-- Modified by: Andrew Seidl <fersla>
+-- http://github.com/fersla/xmonad-config
+-- https://github.com/vicfryzel/xmonad-config
+
+-- This is setup for dual 1920x1080 monitors
+Config {
+    font = "xft:Fixed-8",
+    bgColor = "#002b36",
+    fgColor = "#657b83",
+--    position = Static { xpos = 0, ypos = 0, width = 2560, height = 10 },
+    position = TopW C 100,
+    lowerOnStart = True,
+    commands = [
+        Run Weather "KMSN" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 3600,
+        Run MultiCpu ["-t","Cpu: <autototal>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "wlp3s0" ["-t","wlp3s0: <tx> <rx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "enp0s25" ["-t","enp0s25: <tx> <rx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %multicpu%   %memory%   %wlp3s0%   %enp0s25%   <fc=#FFFFCC>%date%</fc>   %KMSN%"
+}
